0f4c7628bbd56534488d1c5f3d974c0417aadb28
Andrew Lehman <aplehm@gmail.com>
2017-07-29 15:46:32 -0400

    Creat factory for building board

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 (function(chance, Snap, Honeycomb) {
 
-  class SpaceCollection {
+  class Board {
     constructor(Hex) {
       this.Hex = Hex;
       this.map = new Map();
@@ -29,16 +29,16 @@
     }
   }
 
-  class Space {
+  class BoardUnit {
     constructor(hexPath, hexInfo, hexMap) {
-      this.path = hexPath;
-      this.info = hexInfo;
-      this.map = hexMap;
+      this.hexPath = hexPath;
+      this.hexInfo = hexInfo;
+      this.board = hexMap;
       this.setup();
     }
 
     select() {
-      this.path.attr({
+      this.hexPath.attr({
         stroke: 'red',
         strokeWidth: 2
       });
@@ -46,96 +46,94 @@
 
     setup() {
       let hexColor = chance.color({format: 'hex'});
-      this.path.attr({
+      this.hexPath.attr({
         fill: hexColor,
         stroke: '#000'
       });
 
       // events
-      this.path.mouseover(() => {
-        this.path.attr({
+      this.hexPath.mouseover(() => {
+        this.hexPath.attr({
           fill: '#FFF'
         });
       });
 
-      this.path.mouseout(() => {
-        this.path.attr({
+      this.hexPath.mouseout(() => {
+        this.hexPath.attr({
           fill: hexColor
         });
       });
 
-      this.path.click(() => {
-        this.map.selectAdjHexes(this.info);
+      this.hexPath.click(() => {
+        this.board.selectAdjHexes(this.hexInfo);
       });
     }
   }
 
- // console.log(hexes);
-
-  function buildHexRectangle(grid) {
-    const hexCollection = new SpaceCollection(grid.Hex);
-    const rectangle = grid.rectangle({width: 5, height: 5});
-
-    for(let hex of rectangle) {
-      let {x, y, z} = hex;
-      let point = grid.hexToPoint(hex);
-      let hexPoints = determinePolygonPoints({
-        degrees: [30, 90, 150, 210, 270, 330, 390],
-        center: point,
-        radius: 50
-      });
-
-      let hexPath = ctx.path(createPolygon(hexPoints));
-      displayCoords(hex, point);
-      
-      // store hex info
-      hexCollection.setHex(x, y, z, new Space(hexPath, hex, hexCollection));
+  class BoardFactory {
+    constructor(grid) {
+      this.grid = grid;
     }
 
-    return hexCollection;
-  }
+    determineHexagonPoints(params) {
+      let {center, radius} = params;
+      let degrees = [30, 90, 150, 210, 270, 330, 390];
+      let polygonPoints = [];
+      let x, y;
 
-  function determinePolygonPoints(params) {
-    let {degrees, center, radius} = params;
-    let polygonPoints = [];
-    let x, y;
-
-    for (let i = 0; i < degrees.length; i += 1) {
-      x = center.x + radius * Math.cos(degrees[i] * (Math.PI / 180));
-      y = center.y + radius * Math.sin(degrees[i] * (Math.PI / 180));
-      polygonPoints.push({x, y});
-    }
-
-    return polygonPoints;
-  }
-
-  function createPolygon(points) {
-    let firstPoint = true;
-    let polygon = '';
-
-    for (let {x, y} of points) {
-      if (firstPoint) {
-        polygon += `M${x},${y}`;
-        firstPoint = false;
-      } else {
-        polygon += `L${x},${y}`;
+      for (let i = 0; i < degrees.length; i += 1) {
+        x = center.x + radius * Math.cos(degrees[i] * (Math.PI / 180));
+        y = center.y + radius * Math.sin(degrees[i] * (Math.PI / 180));
+        polygonPoints.push({x, y});
       }
+
+      return polygonPoints;
     }
 
-    return polygon;
-  }
+    createHexagon(points) {
+      let firstPoint = true;
+      let polygon = '';
 
-  function displayCoords(hex, center) {
-    let {x, y, z} = hex;
-    ctx.text(center.x - 20, center.y, `${x},${y},${z}`);
+      for (let {x, y} of points) {
+        if (firstPoint) {
+          polygon += `M${x},${y}`;
+          firstPoint = false;
+        } else {
+          polygon += `L${x},${y}`;
+        }
+      }
+
+      return polygon;
+    }
+    
+    build(width, height) {
+      const board = new Board(this.grid.Hex);
+      const rectangle = this.grid.rectangle({width, height});
+
+      for(let hex of rectangle) {
+        let {x, y, z} = hex;
+        let point = this.grid.hexToPoint(hex);
+        let hexPoints = this.determineHexagonPoints({
+          center: point,
+          radius: 50
+        });
+
+        let hexPath = ctx.path(this.createHexagon(hexPoints));
+        
+        // store hex info
+        board.setHex(x, y, z, new BoardUnit(hexPath, hex, board));
+      }
+
+      return board;
+    }
   }
 
   const {Grid, HEX_ORIENTATIONS} = Honeycomb;
   const ctx = Snap("#map");
-  let hexCollection = buildHexRectangle(
-    Grid({size: 50, origin: [-250, -250]})
-  );
+  let gameBoard = 
+    new BoardFactory(Grid({size: 50, origin: [-45, -50]}))
+      .build(10, 10);
 
-  console.log(hexCollection);
+  console.log(gameBoard);
 
 }(chance, Snap, Honeycomb));
